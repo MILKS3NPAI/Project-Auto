ab14ba8fee3afb71f2712b2d920fe5846456328e
kevink02 <kevinkubota0@gmail.com>
2022-02-25 19:29:31 -0800

    Added more buttons

---
+++ project A/Assets/Scripts/ButtonText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ButtonText : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.localPosition = Vector3.zero;
+        transform.localEulerAngles = new Vector3(90, 0, 0);
+    }
+}
